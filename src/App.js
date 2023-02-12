d93490c46ba3e7d28ed3fca934ee717396a09771
AM738349 <96925591+Amirreza10101998@users.noreply.github.com>
2023-02-12 18:06:50 +0000

    .

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import "@fortawesome/react-fontawesome"
+import"@fortawesome/free-solid-svg-icons" 
 import LocationDisplay from './components/LocationDisplay';
 import { Col, Container, Row } from 'react-bootstrap';
+
 
 
 function App() {
   return (
     <Container className='d-flex justify-content-center align-items-center border border-dark rounded' style={{width: '100%', height: '100vh'}}>
       <Row>
-        <Col className='d-flex justify-content-center align-items-center border border-dark rounded p-4' lg={6}><LocationDisplay/></Col>
-        <Col className='d-flex justify-content-center align-items-center border border-dark rounded p-4' lg={6}><LocationDisplay/></Col>
+        <Col className='d-flex justify-content-center align-items-center border border-light rounded p-4'><LocationDisplay/></Col>
       </Row>
     </Container>
 
