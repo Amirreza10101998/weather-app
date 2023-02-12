c04946b7327076aa5141cbeb894238069d713604
AM738349 <96925591+Amirreza10101998@users.noreply.github.com>
2023-02-12 15:35:18 +0000

    basic functionality

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import LocationDisplay from './components/LocationDisplay';
+import { Col, Container, Row } from 'react-bootstrap';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container className='d-flex justify-content-center align-items-center border border-dark rounded' style={{width: '100%', height: '100vh'}}>
+      <Row>
+        <Col className='d-flex justify-content-center align-items-center border border-dark rounded p-4' lg={6}><LocationDisplay/></Col>
+        <Col className='d-flex justify-content-center align-items-center border border-dark rounded p-4' lg={6}><LocationDisplay/></Col>
+      </Row>
+    </Container>
+
   );
 }
-
 export default App;
